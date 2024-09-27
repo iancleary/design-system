c6c7cf7be37f5e2f155ad682621c00c956e67bd2
iancleary <github@iancleary.me>
2024-09-26 21:09:01 -0700

    Add missing license headers

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at https://mozilla.org/MPL/2.0/.
  *
- * Copyright Oxide Computer Company
+ * Copyright Ian Cleary
  */
+
 import { defineConfig } from "tsup";
 
 export default defineConfig({
